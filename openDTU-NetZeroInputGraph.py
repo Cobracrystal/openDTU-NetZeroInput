0ef3742941aa8cf694af577f63fcf00a39b12567
Cobracrystal <Cobracrystal@users.noreply.github.com>
2025-04-05 02:31:31 +0200

    make graph animated and do all three at once

--- openDTU-NetZeroInputGraph.py
+++ openDTU-NetZeroInputGraph.py
@@ -33,23 +33,28 @@ main_inverter = dtu.inverterGetSerial(0)
 max_power = dtu.inverterGetLimitConfig()[main_inverter]['max_power']
 
 graphTime, graphBatteryPower, graphPowerConsumption, graphPowerLimit = [], [], [], []
-figure, axes = plt.subplots()
+figure, axes = plt.subplots(num='Leistung')
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))
-line0, = plt.plot(graphTime, graphBatteryPower, 'r')
-line1, = plt.plot(graphTime, graphPowerConsumption, 'g')
-line2, = plt.plot(graphTime, graphPowerLimit, 'b')
-figure.autofmt_xdate()
-axes.set_ylim(-100, max_power)
+line0, = plt.plot(graphTime, graphBatteryPower, 'g', label='Batterieleistung')
+line1, = plt.plot(graphTime, graphPowerConsumption, 'r', label='Stromverbrauch')
+line2, = plt.plot(graphTime, graphPowerLimit, 'b', label='Wechselrichterlimit')
 
 def init():
-	axes.set_ylim(-100, max_power)
 	figure.autofmt_xdate()
+	axes.set_ylim(-100, max_power)
+	plt.ylabel("Leistung")
+	plt.legend()
 	line0.set_data(graphTime, graphBatteryPower)
 	line1.set_data(graphTime, graphPowerConsumption)
 	line2.set_data(graphTime, graphPowerLimit)
 	return line0, line1, line2
 
 def update(frame):
+	try:
+		update.ticks += 1
+	except:
+		update.ticks = 1
+		update.writeLog = True
 	try:
 		inverter_limit_config = dtu.inverterGetLimitConfig()
 		inverter_runtime_info = dtu.inverterGetRuntimeInfo()
@@ -60,7 +65,7 @@ def update(frame):
 		current_power_delivery = inverter_runtime_info['total']['Power']['v']
 		limit_set_status = inverter_limit_config[main_inverter]['limit_set_status']
 	except Exception as e:
-		print(f'Error has occured while trying to get openDTU data. Waiting 1 minute before retrying. Exception: {repr(e)}')
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Fehler bei Datenabfrage von openDTU. Warte eine Minute vor nächstem Versuch. Fehlernachricht: {repr(e)}')
 		time.sleep(60)
 		return line0, line1, line2,
 	
@@ -68,38 +73,35 @@ def update(frame):
 		bitMeter_data = requests.get(url = f'{urlBitshake}/cm?cmnd=status 10').json()
 		current_power_consumption = bitMeter_data["StatusSNS"]["LK13BE"]["Power"]
 	except Exception as e:
-		print(f'Error has occured while trying to get BitMeter data. Waiting 1 minute before retrying. Exception: {repr(e)}')
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Fehler bei Datenabfrage von BitMeter. Warte eine Minute vor nächstem Versuch. Fehlernachricht: {repr(e)}')
 		time.sleep(60)
 		return line0, line1, line2,
 	
-	if limit_set_status != 'Ok': # If previous change of limit has not been finalized, wait and retry.
-		print(f'Inverter Limit Status is currently {limit_set_status}. Waiting 10 seconds before retrying.')
-		time.sleep(10)
-		return line0, line1, line2,
-
 	if not flagReachable:
-		print(f'Inverter is not reachable')
-		time.sleep(600)
-		return line0, line1, line2,
-
-	old_limit_a = round(old_limit_a)
-	old_limit_r = float(old_limit_r)
-	old_limit_adj_a = old_limit_a // 4 
-	new_limit_a = current_power_consumption + old_limit_adj_a # works even if negative.
-	new_limit_a = new_limit_a * 4
-	new_limit_r = round(100 * new_limit_a / max_power, ndigits=1)
-	if new_limit_a > max_power:
-		new_limit_a = max_power
-		new_limit_r = 100
-	elif new_limit_a  < 0:
-		new_limit_a = 0
-		new_limit_r = 0
-	print(f'Aktueller Stromverbrauch:\t{current_power_consumption}W')
-	print(f'Altes Limit: {old_limit_r}% / {old_limit_a}W. Batterieleistung: {current_power_delivery}W.')
-	print(f'Neues Limit: {new_limit_r}% / {new_limit_a}W ({new_limit_a}/4 = {new_limit_a//4}, {current_power_consumption + current_power_delivery} = {current_power_consumption} + {current_power_delivery})')
-	setLimitResponse = dtu.inverterSetLimitConfig(main_inverter, {"limit_type":0, "limit_value":new_limit_a})
-	if (setLimitResponse['type'] != "success"):
-		print(f'Error when setting new Limit. Error message: {setLimitResponse}')
+		if update.writeLog:
+			print(f'[{datetime.now().strftime("%H:%M:%S")}] Wechselrichter nicht erreichbar. Skippe Logs bis wieder erreichbar.')
+	elif limit_set_status == "Pending":
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Wechselrichter verarbeitet noch das vorherige Limit. Skippe.')
+	else:
+		old_limit_a = round(old_limit_a)
+		old_limit_r = float(old_limit_r)
+		old_limit_adj_a = old_limit_a // 4 
+		new_limit_a = current_power_consumption + old_limit_adj_a # works even if negative.
+		new_limit_a = new_limit_a * 4
+		new_limit_r = round(100 * new_limit_a / max_power, ndigits=1)
+		if new_limit_a > max_power:
+			new_limit_a = max_power
+			new_limit_r = 100
+		elif new_limit_a  < 0:
+			new_limit_a = 0
+			new_limit_r = 0
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Aktueller Stromverbrauch:\t{current_power_consumption}W')
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Altes Limit: {old_limit_r}% / {old_limit_a}W. Batterieleistung: {current_power_delivery}W.')
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Neues Limit: {new_limit_r}% / {new_limit_a}W ({new_limit_a}/4 = {new_limit_a//4}, {current_power_consumption + current_power_delivery} = {current_power_consumption} + {current_power_delivery})')
+		setLimitResponse = dtu.inverterSetLimitConfig(main_inverter, {"limit_type":0, "limit_value":new_limit_a})
+		if (setLimitResponse['type'] != "success"):
+			print(f'[{datetime.now().strftime("%H:%M:%S")}] Fehler beim setzen des Limits. Fehlernachricht: {setLimitResponse}')
+	update.writeLog = flagReachable
 	graphTime.append(datetime.now())
 	graphBatteryPower.append(current_power_delivery)
 	graphPowerConsumption.append(current_power_consumption)
@@ -113,10 +115,11 @@ def update(frame):
 
 try:
 	animation = FuncAnimation(figure, update, blit=False, init_func=init, cache_frame_data=False, interval=standard_interval * 1000)
+	init()
 	plt.show()
 except KeyboardInterrupt:
-	print(f"Programm wird geschlossen. Setze Limit auf 60%...")
+	print(f'[{datetime.now().strftime("%H:%M:%S")}] Programm wird geschlossen. Setze Limit auf 60%...')
 	if dtu.inverterSetLimitConfig(main_inverter, {"limit_type":1, "limit_value":60})['type'] == "Success":
-		print(f"Limit auf 60% gesetzt. Beende..")
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Limit auf 60% gesetzt. Beende..')
 	else:
-		print(f"Limit wurde nicht gesetzt. Beende..")
+		print(f'[{datetime.now().strftime("%H:%M:%S")}] Limit wurde nicht gesetzt. Beende..')
