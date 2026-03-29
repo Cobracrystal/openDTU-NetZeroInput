f1348d85c8d5d6f9bd0bbd721b3739397e9682b8
Cobracrystal <Cobracrystal@users.noreply.github.com>
2026-03-29 23:17:34 +0200

    tiny changes

--- changeConfig.py
+++ changeConfig.py
@@ -23,6 +23,6 @@ manualConfig = {
 }
 LEDBrightness = 10
 config = {"curPin":{"name":"CMT, LEDs, Display","nrf24":{"clk":-1,"cs":-1,"en":-1,"irq":-1,"miso":-1,"mosi":-1},"cmt":{"clk":18,"cs":4,"fcs":5,"sdio":23,"gpio2":19,"gpio3":16},"w5500":{"sclk":-1,"mosi":-1,"miso":-1,"cs":-1,"int":-1,"rst":-1},"eth":{"enabled":False,"phy_addr":0,"power":-1,"mdc":23,"mdio":18,"type":0,"clk_mode":0},"display":{"type":3,"data":21,"clk":22,"cs":255,"reset":255},"led":{"led0":25,"led1":26}},"display":{"rotation":0,"power_safe":True,"screensaver":True,"contrast":6,"locale":"de","diagramduration":18000,"diagrammode":1},"led":[{"brightness":LEDBrightness},{"brightness":LEDBrightness}]}
-print(dtu.deviceSetConfig(config))
-print(f'LED Brightness: {LEDBrightness}. New Config: ')
-print(config)
+# print(dtu.deviceSetConfig(config))
+# print(f'LED Brightness: {LEDBrightness}. New Config: ')
+# print(config)
