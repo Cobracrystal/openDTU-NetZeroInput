d703aaf9c7c932e62ebd5acfed5fb940a8678144
Cobracrystal <Cobracrystal@users.noreply.github.com>
2026-05-07 23:41:56 +0200

    test

--- website/dashboard.py
+++ website/dashboard.py
@@ -204,7 +204,13 @@ def redirect_dashboard():
 @app.route("/dashboard")
 @app.route("/dashboard/main")
 def dashboardMain():
-	return render_template("indexMain.html", active_page="main")
+	return render_template("indexDashboard.html",
+						active_page="dashboard",
+						limits="Unter 50.4V -> 75%, Recovery Buffer: 0.6V (51V)\nUnter 50V -> 50%, Recovery Buffer: 0.7V (50.7V)\nUnter 49.6V -> 0%, Recovery Buffer: 1.7V (51.3V)")
+
+@app.route("/dashboard/grid")
+def dashboardMain():
+	return render_template("indexGrid.html", active_page="grid")
 
 @app.route("/dashboard/individualPower")
 def dashboardIndividualPower():
